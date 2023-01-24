b51d986936c79c895f9c6038416a2ff6254a81c9
J4CKHunter <erdem_nayin@hotmail.com>
2023-01-24 11:52:10 +0300

    jwt-symmetric-key implementation

---
+++ src/main/java/com/erdemnayin/jwt/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.erdemnayin.jwt.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Authentication authentication){
+        return "Hello, " + authentication.getName();
+    }
+
+}
